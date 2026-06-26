9304bccb5c347cc0c58b6b62572b8a0dfb970eba
Shrouk Kamel <shrouk.kamel123@gmail.com>
2026-06-26 07:55:17 +0300

    fix: fix sidebar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,22 +1,23 @@
-import AuthProvider from "@/Features/Dashboard/Guards/AuthInitializer";
-import Navbar from "@/Shared/Components/Navbar/Navbar";
-import SidebarSwitcher from "@/Shared/Components/Sidebar/SidebarSwitcher";
+import AuthProvider from '@/Features/Dashboard/Guards/AuthInitializer';
+import Navbar from '@/Shared/Components/Navbar/Navbar';
+import SidebarSwitcher from '@/Shared/Components/Sidebar/SidebarSwitcher';
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-      <div className="flex h-screen flex-col overflow-hidden bg-surface">
-        <Navbar />
+    <div className="flex min-h-screen flex-col overflow-hidden bg-surface">
+      <Navbar />
 
-        <div className="flex min-h-0 flex-1">
-          <SidebarSwitcher />
+      <div className="flex flex-1 min-h-0">
+        <SidebarSwitcher />
 
-          <main className="min-h-0 flex-1 overflow-y-auto p-4 pb-24 lg:p-6 lg:pb-6">
-            <AuthProvider >
-            {children}
-             </AuthProvider >
-
-          </main>
-        </div>
+        <main className="flex-1 min-h-0 overflow-y-auto p-4 pb-24 lg:p-6 lg:pb-6">
+          <AuthProvider>{children}</AuthProvider>
+        </main>
       </div>
-  )
+    </div>
+  );
 }
